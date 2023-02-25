b845f648e567284511bfda1a250b5f99214abccb
Noth3r <azzakhrufaryannurihsan@gmail.com>
2023-02-26 06:23:12 +0700

    feat - google verif for first time login and merchant register

--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -10,9 +10,14 @@ export const authRouter = createTRPCRouter({
     .input(
       z.object({
         email: z.string().email(),
-        password: z.string().min(6),
-        name: z.string().min(2),
+        password: z
+          .string()
+          .min(6)
+          .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/),
+        firstName: z.string().min(2),
+        lastName: z.string().min(2),
         telepon: z.string().min(10),
+        alamat: z.string().min(10),
       })
     )
     .mutation(async ({ input, ctx }) => {
@@ -21,8 +26,16 @@ export const authRouter = createTRPCRouter({
           data: {
             email: input.email.toLowerCase(),
             password: input.password,
-            name: input.name,
+            name: input.firstName + " " + input.lastName,
+            firstName: input.firstName,
+            lastName: input.lastName,
             telepon: input.telepon,
+            Alamat: {
+              create: {
+                alamat: input.alamat,
+              },
+            },
+            isVerified: true,
           },
         })
         .catch((err) => {
@@ -41,6 +54,76 @@ export const authRouter = createTRPCRouter({
         });
 
       return user;
+    }),
+
+  googleVerification: protectedProcedure
+    .input(
+      z.object({
+        telepon: z.string().min(10),
+        alamat: z.string().min(10),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      await ctx.prisma.user.update({
+        where: {
+          id: ctx.session.user.id,
+        },
+        data: {
+          Alamat: {
+            create: {
+              alamat: input.alamat,
+            },
+          },
+          telepon: input.telepon,
+          isVerified: true,
+        },
+      });
+    }),
+
+  registerMerchant: protectedProcedure
+    .input(
+      z.object({
+        nama: z.string().min(2),
+        alamat: z.string().min(10),
+        jamBuka: z.string().min(4),
+        jamTutup: z.string().min(4),
+        deskripsi: z.string().min(10).nullish(),
+        labels: z.array(z.string()).min(1),
+        dokumen: z.string().min(10),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      await ctx.prisma.user
+        .update({
+          where: {
+            id: ctx.session.user.id,
+          },
+          data: {
+            Merchant: {
+              create: {
+                nama: input.nama,
+                alamat: input.alamat,
+                jamBuka: input.jamBuka,
+                jamTutup: input.jamTutup,
+                deskripsi: input.deskripsi,
+                labels: {
+                  set: input.labels,
+                },
+                dokumen: input.dokumen,
+              },
+            },
+          },
+        })
+        .catch((err) => {
+          if (err instanceof PrismaClientKnownRequestError) {
+            throw new TRPCError({
+              code: "INTERNAL_SERVER_ERROR",
+              message: ErrorCode.InternalServerError,
+            });
+          }
+        });
+
+      return true;
     }),
 
   getAll: publicProcedure.query(({ ctx }) => {
