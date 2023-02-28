7ea9a6edb23ccea9c34505d40b53117500cbe776
Noth3r <azzakhrufaryannurihsan@gmail.com>
2023-03-01 00:35:06 +0700

    fix ts in tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,5 @@
-import { fontFamily } from "tailwindcss/defaultTheme";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
