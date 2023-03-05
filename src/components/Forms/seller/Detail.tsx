1358cbaf643055eb030fa7386d712a3c0708aab6
Noth3r <azzakhrufaryannurihsan@gmail.com>
2023-03-06 04:42:56 +0700

    remove terapkan button

--- src/components/Forms/seller/Detail.tsx
+++ src/components/Forms/seller/Detail.tsx
@@ -1,8 +1,11 @@
-import { useReducer, useState } from "react";
-import { render } from "react-dom";
+import {
+  type ChangeEvent,
+  type KeyboardEvent,
+  useState,
+  useEffect,
+} from "react";
 import { useForm } from "react-hook-form";
-import { useFieldArray } from "react-hook-form/dist/useFieldArray";
-import { FormValues, useFormData } from "../../../context/seller";
+import { type FormValues, useFormData } from "../../../context/seller";
 import Nav from "./Nav";
 
 export default function Detail({
@@ -14,8 +17,7 @@ export default function Detail({
   formStep: number;
   nextFormStep: () => void;
 }) {
-  const { data, setFormValues } = useFormData();
-
+  const { setFormValues } = useFormData();
   const {
     handleSubmit,
     formState: { errors },
@@ -30,24 +32,38 @@ export default function Detail({
 
   const [tempLabel, setTempLabel] = useState("");
   const [labels, setLabels] = useState<string[]>([]);
-  const [, forceupdate] = useReducer((x: number) => x + 1, 0);
 
-  const handleLabelChange = (e: string) => {
-    setTempLabel(() => e);
+  useEffect(() => {
+    register("labels", {
+      required: "Lables wajib diisi minimal 1",
+    });
+  }, [register]);
+
+  const handleLabelChange = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setTempLabel(() => e.target.value);
   };
-  const handleLabelEnter = (e: string) => {
+  const handleLabelEnter = (e: KeyboardEvent<HTMLInputElement>) => {
+    e.key == "Enter" && e.preventDefault();
     // console.log(tempLabel);
-    if (e == " " && tempLabel != " ") {
+    if (e.key == " " && tempLabel != " ") {
       // if (labels[1] === null) tempLabel;
       setLabels((labels) => [...labels, tempLabel.trim()]);
       setTempLabel("");
+      setValue("labels", [...labels, tempLabel.trim()], {
+        shouldValidate: true,
+      });
       // console.log("labels : ", labels);
     }
   };
   const handleLabelDelete = (label: string) => {
-    labels.splice(labels.indexOf(label), 1);
-    setLabels(labels);
-    forceupdate(); // biar kerender ulang, pas hapus salah satu label ga telat hehe
+    setLabels((labels) => labels.filter((l) => l !== label));
+    setValue(
+      "labels",
+      labels.filter((l) => l !== label),
+      { shouldValidate: true }
+    );
+    // forceupdate(); // biar kerender ulang, pas hapus salah satu label ga telat hehe
   };
 
   return (
@@ -95,10 +111,15 @@ export default function Detail({
               type="text"
               autoComplete={"off"}
               value={tempLabel.trim()}
-              onChange={(e) => handleLabelChange(e.target.value)}
-              onKeyDown={(e) => handleLabelEnter(e.key)}
+              onChange={(e) => handleLabelChange(e)}
+              onKeyDown={(e) => handleLabelEnter(e)}
               className="w-2/3 border-b border-others-white bg-transparent py-1 text-center font-louis font-light tracking-wider text-others-white duration-500 focus:border-b focus:border-others-white focus:outline-none"
             />
+            {errors.labels && (
+              <p className="text-error mt-1 text-[12px]">
+                {errors.labels.message}
+              </p>
+            )}
           </div>
         </div>
         <div className="mx-5 mt-5 flex max-h-[30vh] flex-wrap gap-2 overflow-y-auto pr-2">
@@ -134,35 +155,6 @@ export default function Detail({
             </div>
           ))}
         </div>
-        <button
-          type="button"
-          // className="absolute bottom-7 translate-x-28 opacity-100 hue-rotate-180 xs:right-28 xs:translate-x-0"
-          className="text-secondary-50 mt-4 rounded-full border border-primary-300 py-2 px-4 font-louis text-base duration-500 active:bg-primary-300"
-          onClick={() => {
-            setValue("labels", labels);
-          }}
-        >
-          Terapkan label
-          {/* <svg
-            width={28}
-            height={28}
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className="rotate-180"
-          >
-            <rect
-              width={28}
-              height={28}
-              rx={14}
-              transform="matrix(-1 0 0 1 28 0)"
-              fill="#F6C73B"
-            />
-            <path
-              d="M17.587 6.947a1.328 1.328 0 010 1.88L12.413 14l5.174 5.173a1.33 1.33 0 01-1.88 1.88l-6.12-6.12a1.327 1.327 0 010-1.88l6.12-6.12c.506-.506 1.36-.506 1.88.014z"
-              fill="#1D1D1D"
-            />
-          </svg> */}
-        </button>
       </div>
       <Nav prevFormStep={prevFormStep} />
     </form>
