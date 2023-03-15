3e80573c95d8c7cd430a0e38dd8775b4aac3364c
Andyan Yoga <65155073+zenxiety@users.noreply.github.com>
2023-03-15 21:00:35 +0700

    fix - coming soon fade animation

--- src/components/dashboard/ComingSoon.tsx
+++ src/components/dashboard/ComingSoon.tsx
@@ -15,17 +15,17 @@ export default function ComingSoon({
         {children}
       </button>
       <div
-        className={`fixed top-0 m-auto flex h-full w-full max-w-[500px] items-center justify-center overflow-hidden duration-500 ${
-          popup ? "scale-100" : "scale-0 opacity-0"
+        onClick={() => setPopup(false)}
+        className={`fixed inset-0 h-screen w-screen backdrop-blur duration-500 ${
+          popup ? "" : "pointer-events-none opacity-0"
+        }`}
+      />
+      <div
+        className={`fixed inset-0 m-auto flex h-full w-full max-w-[500px] items-center justify-center overflow-hidden duration-500 ${
+          popup ? "" : "pointer-events-none opacity-0"
         }`}
       >
-        <div
-          onClick={() => setPopup(false)}
-          className={`fixed inset-0 h-screen w-screen backdrop-blur delay-500 duration-1000 ${
-            popup ? "" : "opacity-0"
-          }`}
-        />
-        <div className="relative mx-6 rounded-md border-2 border-primary-300 bg-secondary-500 px-12 py-6 text-center delay-200">
+        <div className="relative mx-6 rounded-md border-2 border-primary-300 bg-secondary-500 px-12 py-6 text-center">
           <button
             onClick={() => setPopup(false)}
             className="absolute top-4 left-4"
